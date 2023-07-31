f1a880374d641d5da8aa94fa96f0af04a35b00e7
Tabitha Cat <134290478+tabitta18@users.noreply.github.com>
2023-07-31 19:28:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/admin')
+def index():
+    return render_template('admin.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
