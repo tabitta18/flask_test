dc12fec0f3efce3e379e1808b56ab4c1318bfdab
Tabitha Cat <134290478+tabitta18@users.noreply.github.com>
2023-08-03 17:36:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def admin():
 
 @app.route('/home')
 def index():
-    return render_template('admin.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
